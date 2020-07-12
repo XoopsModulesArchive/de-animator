058fe2ba80672316c5dc2b166c84612950c5ad4d
mambax7 <mambax7@gmail.com>
2020-07-11 22:44:47 -0400

    initial
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include("../../mainfile.php");
+$xoopsOption['template_main'] = "de-animator.html";
+include(XOOPS_ROOT_PATH."/header.php");
+include("header.php");
+
+$xoopsOption['show_rblock'] = 1;
+ ?>
+<html>
+<body>
+<p>
+<a href="http://www.bumlee.com/">De-Animator created Bum Lee</a>
+<p>
+<a href="http://shadowsbastion.com/">Ported to XOOPS by Shadow's Bastion</a>
+</body>
+</html>
+
+
+<?php
+include(XOOPS_ROOT_PATH."/footer.php");
+?>
---
+++ xoops_version.php
@@ -0,0 +1,17 @@
+<?php
+$modversion['name'] = "de-animator";
+$modversion['version'] = 1.00;
+$modversion['description'] = "Flash games hosted on your site.";
+$modversion['author'] = "Ported To Xoops By Shadow (<a href='http://shadowsbastion.com/'>Shadow's Bastion</a>)";
+$modversion['credits'] = "Bum Lee(<a href='http://www.bumlee.com/'>Bumlee.com</a>)";
+$modversion['help'] = "";
+$modversion['license'] = "GPL see LICENSE";
+$modversion['official'] = 0;
+$modversion['image'] = "images/de-animator.jpg";
+$modversion['dirname'] = "de-animator";
+$modversion['hasAdmin'] = 0;
+$modversion['adminpath'] = "";
+$modversion['hasMain'] = 1;
+$modversion['templates'][1]['file'] = "de-animator.html";
+$modversion['templates'][1]['description'] = "The template file of index";
+?>
